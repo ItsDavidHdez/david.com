684257f897b05594a64fa7e3ea1640ba7c59054e
ItsDavidHdez <david-0610@live.com.mx>
2021-04-05 21:54:11 -0500

    fonts and new styles

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 import "./styles/styles.css";
+
+const cursor = document.querySelector("#cursor");
+const anchor = document.querySelectorAll("a");
+const menu = document.querySelector("#menu");
+
+console.log(menu);
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
+
+anchor.forEach((anc) => {
+  anc.addEventListener("mouseover", () => {
+    cursor.style.transform = "scale(2)";
+    cursor.style.animationName = "borderAnim";
+  });
+
+  anc.addEventListener("mouseleave", () => {
+    cursor.style.transform = "";
+  });
+});
